fbc14cb88111604c33ab1a3426fa8ee788c69fe1
Shashank Agarwal <shashank.agarwal@valeo.com>
2025-02-01 13:23:09 +0100

    refactor: breakdown code into functions for streamlit app

--- arxiv_llm.py
+++ arxiv_llm.py
@@ -3,12 +3,9 @@ from langchain_community.docstore.in_memory import InMemoryDocstore
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.chains import RetrievalQA
 from langchain.schema import Document
-from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
-from langchain.llms import HuggingFacePipeline
 from langchain.llms import HuggingFaceHub
 import os
 import faiss
-import pathlib
 import json
 import streamlit as st
 
@@ -17,40 +14,57 @@ os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
 
 DATA_PATH = "./data/arxiv_papers.json"
 
-with open(DATA_PATH, "r") as f:
-    arxiv_papers = json.load(f)
 
-docs = []
-for paper in arxiv_papers:
-    title = paper["title"]
-    abstract = paper["summary"]
-    link = paper["link"]
-    paper_content = f"Title: {title}\nAbstract: {abstract}"
-    paper_content = paper_content.lower()
-
-    docs.append(Document(page_content=paper_content,
-                         metadata={"link": link}))
+def load_json(file_path):
+    with open(file_path, "r") as f:
+        data = json.load(f)
+    return data
 
 
-# Load a pre-trained embedding model
-embedding_model = HuggingFaceEmbeddings(model_name='all-MiniLM-L6-v2')
+def create_documents(data):
+    docs = []
+    for paper in data:
+        title = paper["title"]
+        abstract = paper["summary"]
+        link = paper["link"]
+        paper_content = f"Title: {title}\nAbstract: {abstract}"
+        paper_content = paper_content.lower()
 
-index = faiss.IndexFlatL2(len(embedding_model.embed_query("Hello LLM")))
+        docs.append(Document(page_content=paper_content,
+                             metadata={"link": link}))
 
-vector_store = FAISS(
-    embedding_function=embedding_model,
-    index=index,
-    docstore=InMemoryDocstore(),
-    index_to_docstore_id={},
-)
+    return docs
 
-vector_store.add_documents(docs)
 
-retriever = vector_store.as_retriever()
+def create_vector_db(docs):
+    # Load a pre-trained embedding model
+    embedding_model = HuggingFaceEmbeddings(model_name='all-MiniLM-L6-v2')
 
-llm = HuggingFaceHub(repo_id="tiiuae/falcon-7b-instruct")
+    index = faiss.IndexFlatL2(len(embedding_model.embed_query("Hello LLM")))
 
-qa = RetrievalQA.from_chain_type(llm=llm, retriever=retriever)
+    vector_db = FAISS(
+        embedding_function=embedding_model,
+        index=index,
+        docstore=InMemoryDocstore(),
+        index_to_docstore_id={},
+    )
+
+    vector_db.add_documents(docs)
+
+    return vector_db
+
+
+def create_qa_chain(vector_db):
+    retriever = vector_db.as_retriever()
+    llm = HuggingFaceHub(repo_id="tiiuae/falcon-7b-instruct")
+    qa = RetrievalQA.from_chain_type(llm=llm, retriever=retriever)
+    return qa
+
+
+data = load_json(DATA_PATH)
+docs = create_documents(data)
+vector_db = create_vector_db(docs)
+qa = create_qa_chain(vector_db)
 
 st.title("Arxiv Papers Q&A")
 # User input for query
