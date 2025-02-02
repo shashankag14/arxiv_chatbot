e6c600e9dafc268f6387c4e756de7f9f4a6eb363
Shashank Agarwal <shashank.agarwal@valeo.com>
2025-02-02 14:40:02 +0100

    refactor: create class for the llm building blocks

--- model.py
+++ model.py
@@ -14,109 +14,115 @@ import os
 import streamlit as st
 
 
-def set_api_keys():
-    # Load environment variables from the .env file
-    load_dotenv()
+class ArxivModel:
+    def __init__(self, api_key=None):
 
-    # Check if the API keys are present
-    if not os.getenv('HUGGINGFACE_API_KEY'):
-        raise ValueError(
-            "Error: HUGGINGFACE_API_KEY is not set. Please add it to your .env file.")
-    else:
-        os.environ["HUGGINGFACEHUB_API_TOKEN"] = os.getenv('HUGGINGFACE_API_KEY')
+        if api_key is None:
+            self._set_api_keys()
+        else:
+            os.environ["COHERE_API_KEY"] = api_key
 
-    if not os.getenv('OPENAI_API_KEY'):
-        raise ValueError(
-            "Error: OPENAI_API_KEY is not set. Please add it to your .env file.")
-    else:
-        os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY')
+    def _set_api_keys(self):
+        # Load environment variables from the .env file
+        load_dotenv()
 
-    if not os.getenv('COHERE_API_KEY'):
-        raise ValueError(
-            "Error: OPENAI_API_KEY is not set. Please add it to your .env file.")
-    else:
-        os.environ["COHERE_API_KEY"] = os.getenv('COHERE_API_KEY')
+        # Check if the API keys are present
+        if not os.getenv('HUGGINGFACE_API_KEY'):
+            raise ValueError(
+                "Error: HUGGINGFACE_API_KEY is not set. Please add it to your .env file.")
+        else:
+            os.environ["HUGGINGFACEHUB_API_TOKEN"] = os.getenv('HUGGINGFACE_API_KEY')
 
-    print("API keys loaded successfully!")
+        if not os.getenv('OPENAI_API_KEY'):
+            raise ValueError(
+                "Error: OPENAI_API_KEY is not set. Please add it to your .env file.")
+        else:
+            os.environ["OPENAI_API_KEY"] = os.getenv('OPENAI_API_KEY')
 
+        if not os.getenv('COHERE_API_KEY'):
+            raise ValueError(
+                "Error: OPENAI_API_KEY is not set. Please add it to your .env file.")
+        else:
+            os.environ["COHERE_API_KEY"] = os.getenv('COHERE_API_KEY')
 
-def load_json(file_path):
-    with open(file_path, "r") as f:
-        data = json.load(f)
-    return data
+        print("API keys loaded successfully!")
 
+    def load_json(self, file_path):
+        with open(file_path, "r") as f:
+            data = json.load(f)
+        return data
 
-def create_documents(data):
-    docs = []
-    for paper in data:
-        title = paper["title"]
-        abstract = paper["summary"]
-        link = paper["link"]
-        paper_content = f"Title: {title}\nAbstract: {abstract}"
-        paper_content = paper_content.lower()
+    def create_documents(self, data):
+        docs = []
+        for paper in data:
+            title = paper["title"]
+            abstract = paper["summary"]
+            link = paper["link"]
+            paper_content = f"Title: {title}\nAbstract: {abstract}"
+            paper_content = paper_content.lower()
 
-        docs.append(Document(page_content=paper_content,
-                             metadata={"link": link}))
+            docs.append(Document(page_content=paper_content,
+                                 metadata={"link": link}))
 
-    return docs
+        return docs
 
+    def get_llm(self):
+        return ChatCohere(model="command-r-plus-08-2024",
+                          max_tokens=256,
+                          temperature=0.75
+                          )
 
-def get_llm():
-    return ChatCohere(model="command-r-plus-08-2024", max_tokens=256, temperature=0.75)
+    def get_prompt(self):
+        return hub.pull("rlm/rag-prompt")
 
+    def create_vector_db(self, docs):
+        # Load a pre-trained embedding model
+        embedding_model = CohereEmbeddings(model="embed-english-light-v3.0")
 
-def get_prompt():
-    return hub.pull("rlm/rag-prompt")
+        index = faiss.IndexFlatL2(len(embedding_model.embed_query("Hello LLM")))
 
-
-def create_vector_db(docs):
-    # Load a pre-trained embedding model
-    embedding_model = CohereEmbeddings(model="embed-english-light-v3.0")
-
-    index = faiss.IndexFlatL2(len(embedding_model.embed_query("Hello LLM")))
-
-    vector_db = FAISS(
-        embedding_function=embedding_model,
-        index=index,
-        docstore=InMemoryDocstore(),
-        index_to_docstore_id={},
-    )
-
-    vector_db.add_documents(docs)
-
-    return vector_db
-
-
-def create_qa_chain(vector_db, llm, prompt):
-    retriever = vector_db.as_retriever()
-
-    qa_chain = (
-        {"context": retriever, "question": RunnablePassthrough()}
-        | prompt
-        | llm
-        | StrOutputParser()
+        vector_db = FAISS(
+            embedding_function=embedding_model,
+            index=index,
+            docstore=InMemoryDocstore(),
+            index_to_docstore_id={},
         )
 
-    return qa_chain
+        vector_db.add_documents(docs)
 
+        return vector_db
 
-def get_model(data):
-    docs = create_documents(data)
-    vector_db = create_vector_db(docs)
-    llm = get_llm()
-    prompt = get_prompt()
-    qa_chain = create_qa_chain(vector_db, llm, prompt)
-    return qa_chain
+    def create_qa_chain(self, vector_db, llm, prompt):
+        retriever = vector_db.as_retriever()
+
+        qa_chain = (
+            {"context": retriever, "question": RunnablePassthrough()}
+            | prompt
+            | llm
+            | StrOutputParser()
+            )
+
+        return qa_chain
+
+    def get_model(self, data):
+        docs = self.create_documents(data)
+        vector_db = self.create_vector_db(docs)
+        llm = self.get_llm()
+        prompt = self.get_prompt()
+        qa_chain = self.create_qa_chain(vector_db, llm, prompt)
+        return qa_chain
 
 
 if __name__ == "__main__":
     USE_PRELOADED_DATA = False
     USE_STREAMLIT = False
 
+    arxiv_model = ArxivModel()
+
     # Use preloaded arxiv data in JSON format for RAG
     if USE_PRELOADED_DATA:
         DATA_PATH = "./data/arxiv_papers.json"
-        data = load_json(DATA_PATH)
+        data = arxiv_model.load_json(DATA_PATH)
     # Use direct API URL to parse the data from arxiv using keyword based search
     else:
         KEYWORD = "RAG"
@@ -126,9 +132,7 @@ if __name__ == "__main__":
         feed = feedparser.parse(url)
         data = feed.entries
 
-    set_api_keys()
-
-    qa_chain = get_model(data)
+    qa_chain = arxiv_model.get_model(data)
 
     for chunk in qa_chain.stream("What is LLM?"):
         print(chunk, end="", flush=True)
