8d8d3b39cf360505dda1bd0286e5702d0258d25c
Shashank Agarwal <shashank.agarwal@valeo.com>
2025-02-10 00:22:41 +0100

    draft: use history aware retriever for reformulating input question

--- model.py
+++ model.py
@@ -2,7 +2,9 @@ from langchain_community.vectorstores import FAISS
 from langchain_community.docstore.in_memory import InMemoryDocstore
 from langchain.schema import Document
 from langchain.chains.retrieval import create_retrieval_chain
+from langchain.chains.history_aware_retriever import create_history_aware_retriever
 from langchain_cohere import ChatCohere, CohereEmbeddings
+from langchain_core.prompts import MessagesPlaceholder
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.prompts import ChatPromptTemplate
 
@@ -53,13 +55,7 @@ class ArxivModel:
 
         return docs
 
-    def get_llm(self):
-        return ChatCohere(model="command-r-plus-08-2024",
-                          max_tokens=256,
-                          temperature=0.5
-                          )
-
-    def get_retriever(self, docs):
+    def create_retriever(self, docs):
         # Load a pre-trained embedding model
         embedding_model = CohereEmbeddings(model="embed-english-light-v3.0")
 
@@ -75,7 +71,27 @@ class ArxivModel:
 
         vector_db.add_documents(docs)
 
-        return vector_db.as_retriever()
+        self.retriever = vector_db.as_retriever()
+
+    def create_history_aware_retreiver(self):
+        system_prompt_to_reformulate_input = (
+            "Given a chat history and the latest user question "
+            "which might reference the context in the chat history "
+            "formulate a standalone question which can be understood "
+            "without chat history. Do NOT answer the question, "
+            "just reformulate it is needed and otherwise return it as is."
+        )
+
+        prompt_to_reformulate_input = ChatPromptTemplate.from_messages([
+            ("system", system_prompt_to_reformulate_input),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}")
+        ])
+
+        history_aware_retriever_chain = create_history_aware_retriever(
+            self.llm, self.retriever, prompt_to_reformulate_input
+        )
+        return history_aware_retriever_chain
 
     def get_prompt(self):
         system_prompt = ("You are an AI assistant called 'ArXiv Assist' that has a conversation with the user and answers to questions based on the provided research papers. "
@@ -87,23 +103,34 @@ class ArxivModel:
                          )
         prompt = ChatPromptTemplate.from_messages([
             ("system", system_prompt),
+            MessagesPlaceholder("chat_history"),
             ("human", "Answer the following question: {input}")
         ])
 
         return prompt
 
-    def create_qa_chain(self, retriever, llm):
+    def create_qa_chain(self):
+        # Generate main prompt for the final chain
         prompt = self.get_prompt()
-        qa_chain = create_stuff_documents_chain(llm=llm, prompt=prompt)
+
+        # Subchain 1: Create ``history aware´´ retriever chain that uses conversation history tp update docs
+        history_aware_retriever_chain = self.create_history_aware_retreiver()
+
+        # Subchain 2: Create chain to send docs to LLM
+        qa_chain = create_stuff_documents_chain(llm=self.llm, prompt=prompt)
+
+        # Main chain: Create a chain that connects the two subchains
         rag_chain = create_retrieval_chain(
-            retriever=retriever, combine_docs_chain=qa_chain)
+            retriever=history_aware_retriever_chain,
+            combine_docs_chain=qa_chain)
         return rag_chain
 
     def get_model(self, data):
         docs = self.create_documents(data)
-        retriever = self.get_retriever(docs)
-        llm = self.get_llm()
-        rag_chain = self.create_qa_chain(retriever, llm)
+        self.create_retriever(docs)
+        self.llm = ChatCohere(
+            model="command-r-plus-08-2024", max_tokens=256, temperature=0.5)
+        rag_chain = self.create_qa_chain()
         return rag_chain
 
 
